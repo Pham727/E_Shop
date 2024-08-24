d4dc44be510d3283c43a92737f641aad75c20428
HaPham <ngoha.pham1202@gmail.com>
2024-08-24 16:29:24 +0700

    -Create ui and process to add review for product.

--- e-shop/app/product/[productId]/page.tsx
+++ e-shop/app/product/[productId]/page.tsx
@@ -3,20 +3,23 @@ import ProductDetails from "./ProductDetails";
 import ListRating from "./ListRating";
 import getProductById from "@/actions/getProductById";
 import NullData from "@/app/components/NullData";
+import AddRating from "./AddRating";
+import {getCurrentUser} from "@/actions/getCurrentUser"
 
 interface IPrams {
     productId?: String;
 }
 const Product = async ({params} : {params: IPrams}) => {
     const product = await getProductById(params);
-
+    const user = await getCurrentUser();
+    
     if(!product) return <NullData title ="Opps! Product does not exist"></NullData>
 
     return <div className="p-8">
         <Container>
             <ProductDetails product ={product}/>
             <div className="flex flex-col mt-20 gap-4">
-                <div>Add rating</div>
+                <AddRating product ={product} user ={user} />
                 <ListRating product={product}/>
                 
             </div>
